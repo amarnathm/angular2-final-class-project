5973ea45394753e1a2290cc1e1966afece1f0f1e
am <am@MacBook-Pro.local>
2016-12-24 14:53:34 -0800

    Implemented master/detail

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -11,15 +11,44 @@ import { AjaxService } from './ajax.service';
       <div class="row">
         <div class="column col-sm-6">
         <spinner [visible]="isLoading"> </spinner>
-          <ul class="list-group">
-            <li *ngFor="let post of _posts"
-                        class="list-group-item">{{ post.title }}
+          <ul class="list-group posts">
+            <li *ngFor="let post of _posts; let i = index;" 
+                [id]="post.id"
+                (click)="selectPost2(i)"
+                class="list-group-item"
+                [class.active]="selectedPost?.id == post.id" >{{ post.title }}
             </li>
           </ul>
         </div> <!-- column>
+
+        <!-- detail panel, visible only if something is selected -->
+        <div *ngIf="selectedPost != null"
+            class="column col-sm-6">
+            <!-- bootstrap panel with header markup -->
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">{{ selectedPost?.title }}</h3>
+              </div>
+              <div class="panel-body">
+                {{ selectedPost?.body }}
+              </div>
+            </div>
+        </div>
       </div> <!-- row -->
     </div>
-    `
+    ` ,
+
+    styles: [ `
+        .posts li { cursor: default; }
+        .posts li:hover { background:	#ecf0f1; }	
+        .list-group-item.active,	
+        .list-group-item.active:hover,	
+        .list-group-item.active:focus {	
+	        background-color: #ecf0f1;
+	        border-color: #ecf0f1;	
+	        color: #2c3e50;
+        }
+    `]
 })
 export class PostsComponent implements OnInit, OnDestroy { 
     _posts: Post[];
@@ -29,10 +58,13 @@ export class PostsComponent implements OnInit, OnDestroy {
     subscription;
     isLoading = true;
 
+    selectedPost: Post;
+
     constructor(private _ajaxService: AjaxService) { }
 
     ngOnInit() {
-       this.getPosts();
+        this.getPosts();
+        this.selectedPost = null; 
     }
 
     ngOnDestroy() {
@@ -49,5 +81,14 @@ export class PostsComponent implements OnInit, OnDestroy {
         , () => {} // completed
     }
     
+    selectPost(event) {
+        var target = event.target || event.srcElement || event.currentTarget;
+        var idAttr = target.attributes.id;
+        this.selectedPost = this._posts[2];
+    }
+
+    selectPost2(index) {
+        this.selectedPost = this._posts[index];
+    }
 
 }

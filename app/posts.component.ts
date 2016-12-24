4ed535f6acb49600605ce494a21baed1d052a21d
am <am@MacBook-Pro.local>
2016-12-24 11:08:50 -0800

    Implementing Posts: step 1, display list of posts.

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -1,6 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from './post';
+
+import { AjaxService } from './ajax.service';
 
 @Component({
-    template: ` <h1> Posts </h1>`
+    template: ` <h1> Posts </h1>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="column col-sm-6">
+          <ul class="list-group">
+            <li *ngFor="let post of _posts"
+                        class="list-group-item">{{ post.title }}
+            </li>
+          </ul>
+        </div> <!-- column>
+      </div> <!-- row -->
+    </div>
+    `
 })
-export class PostsComponent { }
+export class PostsComponent implements OnInit, OnDestroy { 
+    _posts: Post[];
+
+    _url = "http://jsonplaceholder.typicode.com/posts";
+
+    subscription;
+
+    constructor(private _ajaxService: AjaxService) { }
+
+    ngOnInit() {
+       this.getPosts();
+    }
+
+    ngOnDestroy() {
+
+    }
+
+    getPosts() {
+        this._ajaxService.get(this._url).subscribe(
+            response => this._posts = <Post[]> response
+        ), error => console.log(error)
+        , () => {} // completed
+    }
+    
+
+}
